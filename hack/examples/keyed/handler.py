3feb4bb304a8533372fd948b61f92493a3a771bc
Marco Ferragina <marco.ferragina@gmail.com>
2021-05-18 17:22:06 +0200

    remove artificial error from example

--- hack/examples/keyed/handler.py
+++ hack/examples/keyed/handler.py
@@ -3,5 +3,4 @@ import random
 def handle_event(context, data):
     context.logger.info(data.decode())
     key = random.randrange(1, 100)
-    ss
     return context.response(key, data.decode() +  " keyed")
