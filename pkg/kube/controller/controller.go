67063384bce93a175920e9e48b0ccf6695edd6f9
Marco Ferragina <marco.ferragina@gmail.com>
2021-06-14 08:39:24 +0200

    moved k8s objects creation on their own factory

--- pkg/kube/controller/controller.go
+++ pkg/kube/controller/controller.go
@@ -10,7 +10,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	appsinformers "k8s.io/client-go/informers/apps/v1"
@@ -434,76 +433,5 @@ func (c *Controller) handleObject(obj interface{}) {
 
 		c.enqueueBruco(bruco)
 		return
-	}
-}
-
-func newService(bruco *brucov1alpha1.Bruco) *corev1.Service {
-	labels := map[string]string{
-		"app":        "bruco",
-		"controller": bruco.Name,
-	}
-	return &corev1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      bruco.Name,
-			Namespace: bruco.Namespace,
-			OwnerReferences: []metav1.OwnerReference{
-				*metav1.NewControllerRef(bruco, brucov1alpha1.SchemeGroupVersion.WithKind("Bruco")),
-			},
-		},
-		Spec: corev1.ServiceSpec{
-			Ports: []corev1.ServicePort{
-				{
-					Port: 8080,
-					TargetPort: intstr.IntOrString{
-						IntVal: 8080,
-					},
-				},
-			},
-			Selector: labels,
-		},
-	}
-}
-
-// newDeployment creates a new Deployment for a Bruco resource. It also sets
-// the appropriate OwnerReferences on the resource so handleObject can discover
-// the Bruco resource that 'owns' it.
-func newDeployment(bruco *brucov1alpha1.Bruco) *appsv1.Deployment {
-	labels := map[string]string{
-		"app":        "bruco",
-		"controller": bruco.Name,
-	}
-	containerImage := "ferama/bruco:dev"
-	if bruco.Spec.ContainerImage != "" {
-		containerImage = bruco.Spec.ContainerImage
-	}
-	return &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      bruco.Name,
-			Namespace: bruco.Namespace,
-			OwnerReferences: []metav1.OwnerReference{
-				*metav1.NewControllerRef(bruco, brucov1alpha1.SchemeGroupVersion.WithKind("Bruco")),
-			},
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: bruco.Spec.Replicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: labels,
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: labels,
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:    "bruco",
-							Image:   containerImage,
-							Command: []string{"bruco", bruco.Spec.FunctionURL},
-							Env:     bruco.Spec.Env,
-						},
-					},
-				},
-			},
-		},
 	}
 }
